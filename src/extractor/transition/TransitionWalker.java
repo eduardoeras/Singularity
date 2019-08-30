15913493d1c98b28ed68c722660f82dd28456446
Eduardo Rohde Eras <eduardorohdeeras@gmail.com>
2019-08-30 13:13:19 -0300

    Transition Extraction first glimpse

--- src/extractor/transition/TransitionWalker.java
+++ src/extractor/transition/TransitionWalker.java
@@ -1,42 +1,56 @@
 package extractor.transition;
 
-import global.structure.Element;
 import global.structure.State;
 import global.structure.Transition;
-import global.structure.Visibility;
-import global.tools.StringTools;
-import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TransitionWalker {
     //Attributes
-    private StringTools stringTools;
+    private List<State> main;
 
     //Constructor
     public TransitionWalker () {
-        stringTools = new StringTools();
+        main = new ArrayList<>();
     }
 
     //Public Methods
     public void walk (List<State> states, List<Transition> transitions) {
         createMainFunction(states);
+        for (State state : main) {
+            findNext(state, states);
+        }
+        main.clear();
     }
 
     //Private Methods
     private void createMainFunction (List<State> states) {
-        List<State> main = new ArrayList<>();
-
         for (State state : states) {
-            if (state.getElement() == Element.CLASS || state.getElement() == Element.STRUCT) {
-                System.out.println("CLASS/SCOPE");
-                System.out.println(state.getLabel());
+            switch (state.getElement()) {
+                case CONSTRUCTOR:
+                case FUNCTION:
+                case OPERATOR:
+                    main.add(state);
+                    break;
+                default:
+                    //do nothing
             }
-            if (state.getElement() == Element.FUNCTION) {
-                System.out.println("FUNCTION");
-                System.out.println(state.getLabel());
-            }
+        }
+    }
+
+    private void findNext(State state, List<State> states) {
+        System.out.println(states.get(states.indexOf(state)).getLabel());
+        switch (state.getElement()) {
+            case FUNCTION:
+            case OPERATOR:
+            case CONSTRUCTOR:
+            case DESTRUCTOR:
+                try {
+                    findNext(states.get(states.indexOf(state) + 1), states);
+                } catch (Exception e){
+                    System.out.println("NOTHING NEXT");
+                }
         }
     }
 
